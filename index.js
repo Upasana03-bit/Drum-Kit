9983cc90e8634054613cde6ed0c832cd8329af3b
Upasana03 <upasanachoudhury2000@gmail.com>
2022-01-28 22:28:00 +0530

    new changes

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ for(var i=0; i<num; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click",function(){
         var buttonInnerHTML = this.innerHTML;
         makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
 
     });
 }
 
 document.addEventListener("keypress",function(event){
     makeSound(event.key);
+    buttonAnimation(event.key);
 });
 
 function makeSound(key){
@@ -46,6 +48,15 @@ function makeSound(key){
         default: 
             console.log(buttonInnerHTML);
     }
+}
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("."+currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+        
+    },100);
 }
 
 
